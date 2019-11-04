662c9076658f65e5bd865a2bcc3bc0a5820a92eb
Jenya Bykov <mothersasg@yandex.ru>
2019-11-04 17:41:08 +0300

    init commit

---
+++ app/src/main/java/com/orange_infinity/onlinepay/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.orange_infinity.onlinepay.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.orange_infinity.onlinepay.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
