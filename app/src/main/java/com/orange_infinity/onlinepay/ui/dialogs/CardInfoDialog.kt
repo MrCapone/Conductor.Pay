8940486d74695a0132c4233deebfb4d0bcc1cbd5
Jenya Bykov <mothersasg@yandex.ru>
2020-01-08 21:42:01 +0500

    Add stopper for card payment

--- app/src/main/java/com/orange_infinity/onlinepay/ui/dialogs/CardInfoDialog.kt
+++ app/src/main/java/com/orange_infinity/onlinepay/ui/dialogs/CardInfoDialog.kt
@@ -1,12 +1,15 @@
 package com.orange_infinity.onlinepay.ui.dialogs
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.app.Dialog
+import android.os.AsyncTask
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.fragment.app.DialogFragment
 import com.orange_infinity.onlinepay.R
 import kotlinx.android.synthetic.main.dialog_card_info.*
+import java.util.concurrent.TimeUnit
 
 class CardInfoDialog : DialogFragment() {
 
@@ -24,10 +27,26 @@ class CardInfoDialog : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val v = LayoutInflater.from(context).inflate(R.layout.dialog_card_info, null)
 
-        tvCardInfo.text = cardInfo
+        //tvCardInfo.text = cardInfo
+        tvCardInfo.text = "Установка связи..."
+        fakeReading()
 
         return AlertDialog.Builder(activity)
             .setView(v)
             .create()
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private fun fakeReading() {
+        object : AsyncTask<Unit, Unit, Unit>() {
+
+            override fun doInBackground(vararg params: Unit) {
+                TimeUnit.MILLISECONDS.sleep(2000)
+            }
+
+            override fun onPostExecute(result: Unit?) {
+                tvCardInfo.text = "Нет связи с оператором"
+            }
+        }
     }
 }
